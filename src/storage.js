91a3ae37a8224b9e75e0ec3d97abcbe4d7bace0a
Tamosauskas <thiago.gt@gmail.com>
2026-09-18 10:27:19 -0300

    Migra saves para decomposição

--- src/storage.js
+++ src/storage.js
@@ -34,6 +34,7 @@ export function deserialize(raw) {
     }
     if (!Number.isInteger(data.pendingEcologicalEvents))
       data.pendingEcologicalEvents = 0;
+    if (!Array.isArray(data.deathSites)) data.deathSites = [];
     delete data.nextEventRound;
     return assertState(data);
   }
