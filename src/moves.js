839be1260f450bd326f1cd4356dcb6ea67983a1c
Tamosauskas <thiago.gt@gmail.com>
2026-09-20 18:43:17 -0300

    refactor: update moves.js for locomotion cleanup

--- src/moves.js
+++ src/moves.js
@@ -245,7 +245,7 @@ export function movesFor(state, p, { ignoreChain = false } = {}) {
       }
     } else ray([...ORTH, ...DIAG], captureOnly);
   }
-  const mobile = has(p, "Locomoção") || has(p, "Locomoção Avançada");
+  const mobile = has(p, "Locomoção Articulada") || has(p, "Locomoção Avançada");
   if (mobile) chessTargets(false);
   else if (has(p, "Predação")) chessTargets(true);
   if (
