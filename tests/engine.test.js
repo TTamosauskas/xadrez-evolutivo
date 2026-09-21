b4a80d6d71d80927a9337f89131572821f3f9afb
Tamosauskas <thiago.gt@gmail.com>
2026-09-21 18:25:12 -0300

    Test pathogen mutation normalization

--- tests/engine.test.js
+++ tests/engine.test.js
@@ -52,6 +52,8 @@ import {
   leaveBacterialTrail,
   exposePathogenCell,
   PATHOGEN_SOMATIC_MUTATION_CHANCE,
+  PRE_REPAIR_PATHOGEN_SOMATIC_MUTATION_CHANCE,
+  pathogenSomaticMutationChance,
 } from "../src/disease.js";
 import {
   reproduce,
@@ -2058,6 +2060,12 @@ test("pathogen exposure can add one non-heritable somatic mutation per outbreak"
     disease = startDisease(s, "eco", s.pieces[1], null, "fungus");
 
   assert.equal(PATHOGEN_SOMATIC_MUTATION_CHANCE, 0.25);
+  assert.equal(PRE_REPAIR_PATHOGEN_SOMATIC_MUTATION_CHANCE, 0.5);
+  assert.equal(pathogenSomaticMutationChance({ traits: [] }), 0.5);
+  assert.equal(
+    pathogenSomaticMutationChance({ traits: ["Reparo Celular"] }),
+    0.25,
+  );
   disease.contaminated.push(36);
   target.pathogenExposureRounds = {};
   target.pathogenMutationDiseases = [];
