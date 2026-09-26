bb02ff2fd24f9b46dc05d0b74fce0f2a7664096f
Tamosauskas <thiago.gt@gmail.com>
2026-09-26 09:17:22 -0300

    Impede Predação de reproduzir em casas férteis

--- src/reproduction-traits.js
+++ src/reproduction-traits.js
@@ -42,6 +42,7 @@ const lineageReached = (piece, trait) =>
 export const canUseBasalFertility = (piece) =>
   !!piece &&
   has(piece, "Respiração anaeróbia") &&
+  !has(piece, "Predação") &&
   !has(piece, "Reprodução Sexuada");
 
 export function predatoryReproductionAvailable(attacker, victim) {
